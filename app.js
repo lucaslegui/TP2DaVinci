5df413835629f5ed83e02e239ced5728d85b88bb
Lucas Leguizamon <lucaslegui1107@gmail.com>
2023-06-24 21:46:39 -0300

    segundo commit

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-
+//array de objetos que continen los productos
 const computadoras = [
+    { nombre: 'PC Escritorio Intel', imagen: 'assets/computadora1.webp', detalles: 'CPU I3 3200Mhz – 8GB DDR3 – SSD 240GB – Sistema Operativo Windows 11', estrellas: 'assets/estrellas.png', precio: 129.999, stock: 120 },
+    { nombre: 'PC Escritorio Intel', imagen: 'assets/computadora1.webp', detalles: 'CPU I5 9600K – 8GB DDR3 – SSD 240GB – Sistema Operativo Windows 11', estrellas: 'assets/estrellas.png', precio: 158394, stock: 90 },
+    { nombre: 'PC Escritorio Intel', imagen: 'assets/computadora1.webp', detalles: 'CPU I5 11600K – 16GB DDR3 – SSD 240GB – Sistema Operativo Windows 11', estrellas: 'assets/estrellas.png', precio: 174299, stock: 97 },
+    { nombre: 'PC Escritorio Intel', imagen: 'assets/computadora1.webp', detalles: 'CPU I7 11Gen – 8GB DDR4 – SSD 240GB – Sistema Operativo Windows 11', estrellas: 'assets/estrellas.png', precio: 199998, stock: 45 },
+    { nombre: 'PC Escritorio Intel', imagen: 'assets/computadora1.webp', detalles: 'CPU Amd Ryzen 5 – 16GB DDR4 – SSD 512GB – Sistema Operativo Windows 11', estrellas: 'assets/estrellas.png', precio: 298999, stock: 15 },
+    { nombre: 'PC Escritorio Intel', imagen: 'assets/computadora1.webp', detalles: 'CPU Intel I7 12700 – 32GB DDR4 – SSD 512GB – Sistema Operativo Windows 11', estrellas: 'assets/estrellas.png', precio: 534798, stock: 2 }
+];
+
+const teclados = [
+    { nombre: 'Teclado T-dagger', imagen: 'assets/teclado1.webp', detalles: 'Teclado Mecanico 60% Gamer Edicion Arena RGB - Color rosa', estrellas: 'assets/estrellas.png', precio: 13.999, stock: 250 },
+    { nombre: 'Teclado T-dagger', imagen: 'assets/teclado1.webp', detalles: 'Teclado Mecanico 60% Gamer Edicion Arena RGB - Color rosa', estrellas: 'assets/estrellas.png', precio: 13.999, stock: 250 },
+    { nombre: 'Teclado T-dagger', imagen: 'assets/teclado1.webp', detalles: 'Teclado Mecanico 60% Gamer Edicion Arena RGB - Color rosa', estrellas: 'assets/estrellas.png', precio: 13.999, stock: 250 },
+    { nombre: 'Teclado T-dagger', imagen: 'assets/teclado1.webp', detalles: 'Teclado Mecanico 60% Gamer Edicion Arena RGB - Color rosa', estrellas: 'assets/estrellas.png', precio: 13.999, stock: 250 },
+    { nombre: 'Teclado T-dagger', imagen: 'assets/teclado1.webp', detalles: 'Teclado Mecanico 60% Gamer Edicion Arena RGB - Color rosa', estrellas: 'assets/estrellas.png', precio: 13.999, stock: 250 },
+    { nombre: 'Teclado T-dagger', imagen: 'assets/teclado1.webp', detalles: 'Teclado Mecanico 60% Gamer Edicion Arena RGB - Color rosa', estrellas: 'assets/estrellas.png', precio: 13.999, stock: 250 }     
+];
+
+const mouse = [
     { nombre: 'PC Escritorio Intel', imagen: 'assets/computadora1.webp', detalles: 'CPU I3 3200Mhz – 8GB DDR3 – SSD 240GB – Sistema Operativo Windows 11', estrellas: 'assets/estrellas.png', precio: 129.999, stock: 120 },
     { nombre: 'PC Escritorio Intel', imagen: 'assets/computadora1.webp', detalles: 'CPU I5 9600K – 8GB DDR3 – SSD 240GB – Sistema Operativo Windows 11', estrellas: 'assets/estrellas.png', precio: 158394, stock: 90 },
     { nombre: 'PC Escritorio Intel', imagen: 'assets/computadora1.webp', detalles: 'CPU I5 11600K – 16GB DDR3 – SSD 240GB – Sistema Operativo Windows 11', estrellas: 'assets/estrellas.png', precio: 174299, stock: 97 },
@@ -11,21 +29,30 @@ const computadoras = [
 const btnMostrar = document.querySelector('#boton-computadoras');
 // btnMostrar.addEventListener('click', mostrarProductos);
 
+btnMostrar.addEventListener('click', function () {
+    if (this.classList.contains('activo')) {
+        let tarjetas = document.querySelector('#mostrandoProductos')
+        while (tarjetas.firstChild) {
+            tarjetas.removeChild(tarjetas.firstChild)
+        }
+        this.classList.remove('activo');
+        this.textContent = "Mostrar";
+    } else {
+        mostrarProductos.call(this); //llama a la funcion
+    }
+});
 
+//funcion para mostrar productos en un div
 function mostrarProductos() {
 
     //cambiar color y texto cuando el boton esta activo
     this.classList.add('activo');
     this.textContent = "X Cerrar";
 
-    //mostrar productos
+    
     let mostrar = document.querySelector('#mostrandoProductos');
 
     for (const computadora of computadoras) {
-
-        //cambia color y texto cuando el boton esta activo        
-        this.classList.add('activo');
-        this.textContent = "X Cerrar";
 
         //cargar productos
         let div = document.createElement('div');
@@ -67,22 +94,6 @@ function mostrarProductos() {
         button.classList.add("btnAgregar");
         div.appendChild(button);
 
-        mostrar.appendChild(div);
+        mostrar.appendChild(div); //mostrar estructura en el div
     }
 }
-
-
-//funcion cerrar boton
-
-btnMostrar.addEventListener('click', function () {
-    if (this.classList.contains('activo')) {
-        let tarjetas = document.querySelector('#mostrandoProductos')
-        while (tarjetas.firstChild) {
-            tarjetas.removeChild(tarjetas.firstChild)
-        }
-        this.classList.remove('activo');
-        this.textContent = "Mostrar";
-    } else {
-        mostrarProductos.call(this);
-    }
-});
